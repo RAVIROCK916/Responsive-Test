dbc9b36819e7795e95a91de507de5b16b1179677
Ravi Teja Pedapudi <prt15@iitbbs.ac.in>
2024-09-19 19:33:19 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Stopwatch from "./components/StopWatch";
+
+import Switches from "./sections/Switches";
+
+function App() {
+	return (
+		<main>
+			<Stopwatch />
+			<Switches />
+		</main>
+	);
+}
+
+export default App;
