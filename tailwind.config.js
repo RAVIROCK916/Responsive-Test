b9897a798fdd084507283ab44ca6438740dd29c6
Ravi Teja Pedapudi <prt15@iitbbs.ac.in>
2024-09-19 19:31:15 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				time: ["Digital-7", "sans-serif"],
+			},
+		},
+	},
+	plugins: [],
+};
